5bf9ef8bb2a894ccfcd80d16889b8a411e07b88a
Reeyan12345 <reyann.11b@gmail.com>
2024-05-13 16:39:56 +0300

    Main.java

--- mid/Q6/src/Main.java
+++ mid/Q6/src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+ublic class Main {
+    public static int[] deleteElement(int[] array, int index) {
+  if (index < 0 || index >= array.length) {
+    System.out.println("Invalid index.");
+    return array;
+  }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+  int[] newArray = new int[array.length - 1];
+  for (int i = 0, j = 0; i < array.length; i++) {
+    if (i == index) {
+      continue;
+    }
+    newArray[j++] = array[i];
+  }
+  return newArray;
+}
+    public static void main(String[] args) {
+
+
+
+int[] arr = {3, 7, 1, 9, 4};
+int[] newArr = deleteElement(arr, 2);
+System.out.println(Arrays.toString(newArr)); // Output: [3, 7, 9, 4]
     }
 }
